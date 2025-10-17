ad1c747aa571f08633fc6a3f84129208fefe93e9
akshaypareek01 <akshay96102@gmail.com>
2025-10-17 11:00:19 +0530

    wokring

--- src/Pages/ZoomCdn.jsx
+++ src/Pages/ZoomCdn.jsx
@@ -15,7 +15,7 @@ const ZoomCdn = () => {
   var sdkKey = 'TsFvuPFLTeKf7_bNBWggPA'
   var meetingNumber =ZoomMeetingNumber.number
   var passWord = ZoomMeetingNumber.pass
-  var role = 0
+  var role = ZoomMeetingNumber.role
   var userName = ZoomMeetingNumber.userName
   var userEmail = ZoomMeetingNumber.email
   var registrantToken = ''
